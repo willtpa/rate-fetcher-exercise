484a245e7612f87bb259c5e21f03513a73da6817
willtpa <will@wills-MacBook-Pro.local>
2024-05-08 15:57:00 +0800

    fixing

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,59 +1,12 @@
-<script>
-	import { connect, StringCodec } from 'nats';
-	const servers = [
-		{},
-		{ servers: ["localhost:4442", "localhost:4222"] },
-		{ port: 4222 },
-		{ servers: "localhost" },
-	];
-  var data;
-	
-	servers.forEach(async (v) => {
-		try {
-			const nc = await connect(v);
-			console.log(`connected to ${nc.getServer()}`);
+<script lang="ts">
+  import type { PageData } from './$types';
+  export let pageData:PageData
 
-			const sc = StringCodec();
-      // create a simple subscriber and iterate over messages
-      // matching the subscription
-			const sub = nc.subscribe("*");
-			await (async () => {
-				for await (const m of sub) {
-					console.log(`[${sub.getProcessed()}]: ${sc.decode(m.data)}`);
-          data = m.data;
-				}
-				console.log("subscription closed");
-			})();
-
-			// this promise indicates the client closed
-			const done = nc.closed();
-			// do something with the connection
-
-			// close the connection
-			await nc.close();
-			// check if the close was OK
-			const err = await done;
-			if (err) {
-				console.log(`error closing:`, err);
-			}
-		} catch (err) {
-			console.log(`error connecting to ${JSON.stringify(v)}`);
-		}
-	});
+  console.log('test', pageData)
 </script>
 
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <button class="btn btn-primary">Hello daisyUI</button>
-<p>{data}</p>
-
-
-
-
-
-
-
-
-
-
+<p>{@html pageData}</p>
---
+++ app/src/routes/+page.ts
@@ -0,0 +1,53 @@
+import type { PageLoad } from './$types';
+import { connect, JSONCodec, StringCodec } from 'nats';
+
+export const load = (async ({ url }) => {
+  // insert code here
+
+
+	const servers = [
+		// {},
+		// { servers: ["localhost:4442", "localhost:4222"] },
+		{ port: 4222 },
+		{ servers: "localhost" },
+	];
+  let datas;
+	
+	servers.forEach(async (v) => {
+		try {
+			const nc = await connect(v);
+			console.log(`connected to ${nc.getServer()}`);
+
+			const sc = StringCodec();
+      // const sc = JSONCodec();
+      // create a simple subscriber and iterate over messages
+      // matching the subscription
+			const sub = nc.subscribe("*");
+			await (async () => {
+				for await (const m of sub) {
+					console.log(`[${sub.getProcessed()}]: ${sc.decode(m.data)}`);
+          // datas = datas.push(sc.decode(m.data));
+				}
+				console.log("subscription closed");
+			})();
+
+			// this promise indicates the client closed
+			const done = nc.closed();
+			// do something with the connection
+
+			// close the connection
+			await nc.close();
+			// check if the close was OK
+			const err = await done;
+			if (err) {
+				console.log(`error closing:`, err);
+			}
+		} catch (err) {
+			console.log(`error connecting to ${JSON.stringify(v)}`);
+		}
+	});
+    return {
+       // whatever we want to return
+       data: datas
+    };
+}) satisfies PageLoad; // PageLoad -> PageData (in our frontend, which is +page.svelte)
