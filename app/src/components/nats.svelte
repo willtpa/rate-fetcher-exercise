bbd331cd98ef5e03d9bcbf222ca31cf8e5a874d3
will <will@wills-MacBook-Pro.local>
2024-05-08 15:14:06 +0800

    chore

---
+++ app/src/components/nats.svelte
@@ -0,0 +1,18 @@
+
+// <script>
+// const updatesDiv = document.getElementById('updates');
+
+// // Connect to the NATS streaming server
+// const nc = nats.connect({
+//   servers: ['nats://localhost:4222'] // Replace 'localhost' with your server IP if needed
+// });
+
+// // Subscribe to a subject
+// const subscription = nc.subscribe('updates');
+
+// // Message handler
+// subscription.on('message', (msg) => {
+//   updatesDiv.innerHTML += msg.data + '<br>';
+
+// </script>
+
