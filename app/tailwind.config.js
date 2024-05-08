ed4b1b060b4be8cda697893ba42a7c57a31c68df
will <will@wills-MacBook-Pro.local>
2024-05-08 13:57:53 +0800

    init

---
+++ app/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // content: [],
+  content: ['./src/**/*.{html,svelte,js,ts}'],
+  theme: {
+    extend: {},
+  },
+  // plugins: [],
+  plugins: [require('daisyui')],
+ }
+ 
